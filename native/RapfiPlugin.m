198aa70a0684777339acdb8d082089b63232b088
Lin-13711113231 <13711113231@users.noreply.github.com>
2026-09-08 12:59:28 +0800

    Compile native Capacitor plugin without umbrella modules

--- native/RapfiPlugin.m
+++ native/RapfiPlugin.m
@@ -1,12 +1,15 @@
-#if defined(CAPACITOR_HEADER_PATH)
-#import CAPACITOR_HEADER_PATH
-#else
-#import <Capacitor/Capacitor.h>
-#endif
+// Do not import the umbrella Capacitor.h here. It imports headers which use
+// Xcode's module-enabled Pod environment (including Cordova), while this
+// small adapter is compiled separately by CMake. These four Objective-C
+// headers are sufficient for a CAPBridgedPlugin and work as plain headers.
+#import "CAPPlugin.h"
+#import "CAPPluginCall.h"
+#import "CAPBridgedPlugin.h"
+#import "CAPPluginMethod.h"
 
 #import "RapfiEngineBridge.h"
 
-@interface RapfiPlugin : CAPPlugin
+@interface RapfiPlugin : CAPPlugin <CAPBridgedPlugin>
 @end
 
 static void RapfiLineReceived(const char *line, void *context)
@@ -22,6 +25,30 @@ static void RapfiLineReceived(const char *line, void *context)
 
 @implementation RapfiPlugin
 
+- (NSString *)identifier
+{
+    return @"RapfiPlugin";
+}
+
+- (NSString *)jsName
+{
+    return @"Rapfi";
+}
+
+- (NSArray<CAPPluginMethod *> *)pluginMethods
+{
+    return @[
+        [[CAPPluginMethod alloc] initWithName:@"start" returnType:CAPPluginReturnPromise],
+        [[CAPPluginMethod alloc] initWithName:@"send" returnType:CAPPluginReturnPromise],
+        [[CAPPluginMethod alloc] initWithName:@"stop" returnType:CAPPluginReturnPromise],
+    ];
+}
+
+- (void)resolve:(CAPPluginCall *)call
+{
+    if (call.successHandler) call.successHandler(nil, call);
+}
+
 - (void)start:(CAPPluginCall *)call
 {
     NSString *configPath = [[NSBundle mainBundle] pathForResource:@"config"
@@ -34,34 +61,32 @@ static void RapfiLineReceived(const char *line, void *context)
     }
 
     if (!configPath || !rapfi_engine_start(configPath.UTF8String, RapfiLineReceived, (__bridge void *)self)) {
-        [call reject:@"Native Rapfi could not start"];
+        [self notifyListeners:@"line" data:@{ @"line": @"ERROR Native Rapfi could not start" }];
+        [self resolve:call];
         return;
     }
 
-    [call resolve:@{ @"engine": @"rapfi-native", @"threads": @YES }];
+    [self resolve:call];
 }
 
 - (void)send:(CAPPluginCall *)call
 {
-    NSString *command = [call getString:@"command"];
+    id value = call.options[@"command"];
+    NSString *command = [value isKindOfClass:[NSString class]] ? value : nil;
     if (command.length == 0) {
-        [call reject:@"Missing command"];
+        [self notifyListeners:@"line" data:@{ @"line": @"ERROR Missing command" }];
+        [self resolve:call];
         return;
     }
 
     rapfi_engine_send(command.UTF8String);
-    [call resolve];
+    [self resolve:call];
 }
 
 - (void)stop:(CAPPluginCall *)call
 {
     rapfi_engine_stop();
-    [call resolve];
+    [self resolve:call];
 }
 
 @end
-
-CAP_PLUGIN(RapfiPlugin, "Rapfi",
-           CAP_PLUGIN_METHOD(start, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(send, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(stop, CAPPluginReturnPromise);)
